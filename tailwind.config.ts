a4911440a31ce22bea3056a87666e931b0cb426c
Muhmmad-Sufyan <144341878+Muhmmad-Sufyan@users.noreply.github.com>
2024-03-22 19:18:37 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      'xs': '576px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+          'mob':'370px',
+      'lg': '1024px',
+      'mlg': '1089px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      }
+      
+    },
+  },
+  plugins: [],
+};
+export default config;
